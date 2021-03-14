8e36056bedca9d132267061b1e4c4595021a6a8c
nf-test104 <nf-test104>
2021-03-14 00:19:38 +0000

    Moved testing script to a specific folder

---
+++ testing-scripts/test_plotter.py
@@ -0,0 +1,142 @@
+print("Plotter")
+import subprocess
+from io import StringIO
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+payload_sizes = [10, 100, 1000, 10000] #cd  , 100000, 1000000, 10000000, 100000000, 1000000000, 4294967295]
+
+def perform_measurement(parameters):
+    print("parameters: ", parameters)
+    command_output = subprocess.run(parameters, stdout=subprocess.PIPE, text=True).stdout
+    # print("command_output:")
+    # print(command_output)
+
+    total_delay_ms           = 0
+    requests_per_second      = 0
+    throughput_KB_per_second = 0
+
+    output_lines = StringIO(command_output)
+    for line in output_lines:
+        if "finished in" in line:
+            print(line)
+            measurements = line.replace('finished in ', '').replace('\n', '').replace(' ', '').split(',')
+            # print(measurements)
+
+            if "ms" == measurements[0][-2:]:
+                total_delay_ms       = float(measurements[0].replace('ms', ''))
+            elif "s" == measurements[0][-1:]:
+                total_delay_ms       = 1000*float(measurements[0].replace('s', ''))
+            requests_per_second      = float(measurements[1].replace('req/s', ''))
+            throughput_KB_per_second = float(measurements[2][:-4])
+            if "MB/s" == measurements[2][-4:]:
+                throughput_KB_per_second *= 1024
+            
+            print(total_delay_ms, "ms")
+            print(requests_per_second, "req/s")
+            print(throughput_KB_per_second, "KB/s")
+
+            return ({"total_delay_ms": total_delay_ms,
+                    "requests_per_second": requests_per_second, 
+                    "throughput_KB_per_second": throughput_KB_per_second})
+
+ 
+def perform_group_of_measurements(parameter_list_prefix, parameter_list_suffix):
+    delays      = []
+    requests    = []
+    throughputs = []
+
+    for payload_size in payload_sizes:
+        url = "https://10.2.2.101:7777/" + str(payload_size)
+        print(url)
+
+        parameters = parameter_list_prefix.copy()
+        parameters.extend(["../../nghttp2/src/h2load", "--npn-list", "h3", url])
+        parameters.extend(parameter_list_suffix)
+        print("parameters: ", parameters)
+
+        measurements = perform_measurement(parameters)
+        delays.append(measurements['total_delay_ms'])
+        requests.append(measurements['requests_per_second'])
+        throughputs.append(measurements['throughput_KB_per_second'])
+
+    print(delays)
+
+    return ({"delays": delays,
+        "requests": requests, 
+        "throughputs": throughputs})
+
+
+
+
+
+# Data for plotting
+
+# fig, (ax1, ax2, ax3) = plt.subplots(3)
+# ax1.plot(payload_sizes, delays)
+# ax2.plot(payload_sizes, requests)
+# ax3.plot(payload_sizes, throughputs)
+
+# ax1.set(xlabel='Payload size', ylabel='total_delay_ms')
+# ax1.grid()
+# ax2.set(xlabel='Payload size', ylabel='requests_per_second')
+# ax2.grid()
+# ax3.set(xlabel='Payload size', ylabel='throughput_KB_per_second')
+# ax3.grid()
+
+
+# # GSO impact
+# measurements_with_gso    = perform_group_of_measurements([])
+# measurements_without_gso = perform_group_of_measurements(["--no-udp-gso"])
+
+# print(payload_sizes)
+# print(measurements_with_gso['throughputs'])
+
+# print(payload_sizes)
+# print(measurements_without_gso['throughputs'])
+
+# fig, ax = plt.subplots(1)
+# ax.plot(payload_sizes, measurements_with_gso['throughputs'],    label='With GSO')
+# ax.plot(payload_sizes, measurements_without_gso['throughputs'], label='No GSO')
+# ax.set(xlabel='Payload size', ylabel='throughput_KB_per_second')
+# ax.grid()
+
+# fig.savefig("test.png")
+# plt.xscale("log")
+# plt.title('GSO impact on throughput')
+
+# # From https://stackoverflow.com/questions/22263807/how-is-order-of-items-in-matplotlib-legend-determined
+# handles, labels = ax.get_legend_handles_labels()
+# labels, handles = zip(*sorted(zip(labels, handles), key=lambda t: t[0]))
+# ax.legend(handles, labels)
+
+# plt.show()
+
+
+
+measurements_on_same_cores       = perform_group_of_measurements(["taskset", "0x1", "ip", "netns", "exec", "mr_client"], []) #[]
+measurements_on_different_cores  = perform_group_of_measurements(["taskset", "0x2", "ip", "netns", "exec", "mr_client"], []) #[]
+
+print(payload_sizes)
+print(measurements_on_same_cores['throughputs'])
+print(measurements_on_different_cores['throughputs'])
+
+fig, ax = plt.subplots(1)
+ax.plot(payload_sizes, measurements_on_same_cores['throughputs'],    label='Client and server on the same core')
+ax.plot(payload_sizes, measurements_on_different_cores['throughputs'],    label='Client and server on different cores')
+ax.set(xlabel='Payload size', ylabel='throughput_KB_per_second')
+ax.grid()
+
+fig.savefig("test.png")
+plt.xscale("log")
+plt.title('Throughput via node B (A-to-B-to-A)')
+
+# From https://stackoverflow.com/questions/22263807/how-is-order-of-items-in-matplotlib-legend-determined
+handles, labels = ax.get_legend_handles_labels()
+labels, handles = zip(*sorted(zip(labels, handles), key=lambda t: t[0]))
+ax.legend(handles, labels)
+
+plt.show()
