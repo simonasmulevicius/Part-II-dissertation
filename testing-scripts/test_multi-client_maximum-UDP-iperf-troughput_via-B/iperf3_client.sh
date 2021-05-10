47e4fdd0b6628a00840d2a9f285cea2cc4fd5405
nf-test104 <nf-test104>
2021-05-11 00:31:08 +0100

    Improved multi-client_maximum-UDP-iperf-troughput_via-B test

--- testing-scripts/test_multi-client_maximum-UDP-iperf-troughput_via-B/iperf3_client.sh
+++ testing-scripts/test_multi-client_maximum-UDP-iperf-troughput_via-B/iperf3_client.sh
@@ -3,40 +3,16 @@ source env.sh
 
 number_of_experiments=10 #
 draw_a_diagram=0
-
-# $1 - receiver_throughput
-# $2 - receiver_throughput_units
-convert_throughput_to_MBytes_per_second() {
-
-    return_receiver_throughput_megabytes_per_second="$1"
-    if [ "$2" == "Gbits/sec" ] 
-    then
-        # echo "Units:Gbits/sec?"
-        return_receiver_throughput_megabytes_per_second=$(bc -l <<<"$1/8*1024*1024*1024/1000/1000")
-    elif [ "$2" == "Mbits/sec" ] 
-    then
-        # echo "Units:Mbits/sec?"
-        return_receiver_throughput_megabytes_per_second=$(bc -l <<<"$1/8*1024*1024/1000/1000")
-    elif [ "$2" == "Kbits/sec" ] || [ "$1" == "kbits/sec" ]
-    then
-        # echo "Units:Kbits/sec?"
-        return_receiver_throughput_megabytes_per_second=$(bc -l <<<"$1/8*1024/1000/1000")
-    else
-        # echo "Units:bits/sec?"
-        return_receiver_throughput_megabytes_per_second=$(bc -l <<<"$1/8/1000/1000")
-    fi
-    echo "${return_receiver_throughput_megabytes_per_second}"
-}
-
+ngtcp2_servers_folder="/root/evaluation/unencrypted_stack/ngtcp2/examples/servers_folder"
 
 
 for delay_ms in 0 #1 10 100
 do
     for probability_of_loss in 0 #0.0001 0.001 0.01 0.1 1 
     do
-        for payload_size in 1000000 10000000 100000000 1000000000
+        for payload_size in 1000000 10000000 100000000 1000000000 #1000000 10000000 100000000 1000000000
         do
-            for clients_requests in 1 10 # 10 
+            for clients_requests in 1 10 # 1 10 
             do
                 group_experiment_description="payload-size_${payload_size}____delay-ms_${delay_ms}____loss-rate-percent_${probability_of_loss}____clients-requests_${clients_requests}"
                 experiment_group_folder="./results/${group_experiment_description}"
@@ -64,87 +40,92 @@ do
                     echo "  individual_experiment_folder: ${individual_experiment_folder}"
 
 
+                    additional_parameters_for_drawing=""
                     if [ ${draw_a_diagram} == 1 ] 
                     then
-                        printf "  1.1 Run throughput test\n"
-                        command=(        'taskset -c 1 ip netns exec mr_client iperf3 -c 10.2.2.101 --udp -b 0 --bytes "${payload_size}" --parallel "${clients_requests}" --interval 0.1' )
-                                            
-                        printf "   1.1.1 Running command: '${command[@]}' \n"
-                        command_output=$( taskset -c 1 ip netns exec mr_client iperf3 -c 10.2.2.101 --udp -b 0 --bytes "${payload_size}" --parallel "${clients_requests}"  --interval 0.1 | tee "${individual_experiment_folder}/result.txt" )
-                    else
-                        printf "  1.1 Run throughput test\n"
-                        command=(        'taskset -c 1 ip netns exec mr_client iperf3 -c 10.2.2.101 --udp -b 0 --bytes "${payload_size}" --parallel "${clients_requests}"' )
-                                            
-                        printf "   1.1.1 Running command: '${command[@]}' \n"
-                        command_output=$( taskset -c 1 ip netns exec mr_client iperf3 -c 10.2.2.101 --udp -b 0 --bytes "${payload_size}" --parallel "${clients_requests}"  | tee "${individual_experiment_folder}/result.txt" )
+                        additional_parameters_for_drawing="--interval 0.1"
                     fi
 
-                    printf "  --------------------------------\n"
-                    printf "  2 Analyse output \n"
-                    printf "  --------------------------------\n"
-                    printf "  2.1 Extract relevant fields \n"
-                    printf "  --------------------------------\n"
-                    relevant_fields=$( tail -3 "${individual_experiment_folder}/result.txt")
-                    echo "${relevant_fields}"
-                    printf "  --------------------------------\n"
+                    printf "  1.1 Run throughput test\n"
+                    command=(        'taskset -c 1 ip netns exec mr_client /root/evaluation/iperf/src/iperf3 -c 10.2.2.101 --udp -b 0 --bytes "${payload_size}" --parallel "${clients_requests}" --json ${additional_parameters_for_drawing}' )
+                                        
+                    printf "   1.1.1 Running command: '${command[@]}' \n"
+                    command_output=$( taskset -c 1 ip netns exec mr_client /root/evaluation/iperf/src/iperf3 -c 10.2.2.101 --udp -b 0 --bytes "${payload_size}" --parallel "${clients_requests}" --json  ${additional_parameters_for_drawing} > "${individual_experiment_folder}/result.json" )
+                   
 
-
-                    receiver_throughput=$( echo ${relevant_fields:36:6} | tr -d ' ' )
-                    receiver_throughput_units=$( echo ${relevant_fields:42:10} | tr -d ' ' )
-                    # Xbits/second
-                    printf "    receiver_throughput                      : [${receiver_throughput}] \n"
-                    printf "    receiver_throughput_units                : [${receiver_throughput_units}] \n"
-
-                    receiver_throughput_megabytes_per_second=$( convert_throughput_to_MBytes_per_second ${receiver_throughput} ${receiver_throughput_units}) 
-
-                    echo "    receiver_throughput_megabytes_per_second : [${receiver_throughput_megabytes_per_second}] (megaBytes/s) "
-                    echo "${receiver_throughput_megabytes_per_second}" > "${individual_experiment_folder}/receiver_throughput_megabytes_per_second.txt"
-
-
-                    receivers_results_summary=$( echo "${relevant_fields}" | grep "receiver" | tail  -1 )
-                    completion_time=$( echo ${receivers_results_summary:13:7} | tr -d ' ' )
-                    echo "    completion_time                          : [${completion_time}] (sec)"
-                    echo "${completion_time}" > "${individual_experiment_folder}/completion_time.txt"
+                    # cat "${individual_experiment_folder}/result.json"
 
 
 
+                    # TODO move to visualisation section
 
 
-                    if [ ${draw_a_diagram} == 1 ] 
-                    then
-                        # Some commands follow instructions from: https://stackoverflow.com/questions/13380607/how-to-use-sed-to-remove-the-last-n-lines-of-a-file
-                        if [ ${clients_requests} == 1 ] 
-                        then
-                            echo "  --------------------------------"
-                            echo "  command_output:"
-                            echo "${command_output}"
-                            echo "  --------------------------------"
+                    # printf "  --------------------------------\n"
+                    # printf "  2 Analyse output \n"
+                    # printf "  --------------------------------\n"
+                    # printf "  2.1 Extract relevant fields \n"
+                    # printf "  --------------------------------\n"
+                    # relevant_fields=$( tail -3 "${individual_experiment_folder}/result.txt")
+                    # echo "${relevant_fields}"
+                    # printf "  --------------------------------\n"
 
-                            diagram_title="UDP throughput (1 client) when transfering ${payload_size} Bytes"
-                            individual_throughput_samples=$( grep " sec " "${individual_experiment_folder}/result.txt"  | sed '$d' | sed '$d' | tr - " " | awk '{
-                                if ($9 =="Gbits/sec")
-                                    {print $3, $8*1024*1024*1024/1000/1000}
 
-                                else if ($9 =="Mbits/sec")
-                                    {print $3, $8*1024*1024/1000/1000}
+                    # receiver_throughput=$( echo ${relevant_fields:36:6} | tr -d ' ' )
+                    # receiver_throughput_units=$( echo ${relevant_fields:42:10} | tr -d ' ' )
+                    # # Xbits/second
+                    # printf "    receiver_throughput                      : [${receiver_throughput}] \n"
+                    # printf "    receiver_throughput_units                : [${receiver_throughput_units}] \n"
 
-                                else if ($9 =="Kbits/sec")
-                                    {print $3, $8*1024/1000/1000}
+                    # receiver_throughput_megabytes_per_second=$( convert_throughput_to_MBytes_per_second ${receiver_throughput} ${receiver_throughput_units}) 
 
-                                else 
-                                    {print $3, $8*/1000/1000}
-                                }' |
-                                gnuplot -p -e 'set title "${diagram_title}" ;  set xlabel "Time (s)"; set ylabel "Throughput over the interval (MBits/s)" ;  set yrange[0:4800] ; plot  "/dev/stdin" title "iperf3 UDP throughput" with linespoints'        )
-                            echo ${individual_throughput_samples} > "${individual_experiment_folder}/throughput_samples_throughout_time.txt"
-                            echo "  individual_throughput_samples:"
-                            echo "${individual_throughput_samples}"
-                            echo "  --------------------------------"
+                    # echo "    receiver_throughput_megabytes_per_second : [${receiver_throughput_megabytes_per_second}] (megaBytes/s) "
+                    # echo "${receiver_throughput_megabytes_per_second}" > "${individual_experiment_folder}/receiver_throughput_megabytes_per_second.txt"
 
-                        fi
-                    fi
 
-                    printf "  --------------------------------\n"
-                    printf "\n\n\n"
+                    # receivers_results_summary=$( echo "${relevant_fields}" | grep "receiver" | tail  -1 )
+                    # completion_time=$( echo ${receivers_results_summary:15:7} | tr -d ' ' )
+                    # echo "    completion_time                          : [${completion_time}] (sec)"
+                    # echo "${completion_time}" > "${individual_experiment_folder}/completion_time.txt"
+
+
+
+
+
+                    # if [ ${draw_a_diagram} == 1 ] 
+                    # then
+                    #     # Some commands follow instructions from: https://stackoverflow.com/questions/13380607/how-to-use-sed-to-remove-the-last-n-lines-of-a-file
+                    #     if [ ${clients_requests} == 1 ] 
+                    #     then
+                    #         echo "  --------------------------------"
+                    #         echo "  command_output:"
+                    #         echo "${command_output}"
+                    #         echo "  --------------------------------"
+
+                    #         diagram_title="UDP throughput (1 client) when transfering ${payload_size} Bytes"
+                    #         individual_throughput_samples=$( grep " sec " "${individual_experiment_folder}/result.txt"  | sed '$d' | sed '$d' | tr - " " | awk '{
+                    #             if ($9 =="Gbits/sec")
+                    #                 {print $3, $8*1024*1024*1024/1000/1000}
+
+                    #             else if ($9 =="Mbits/sec")
+                    #                 {print $3, $8*1024*1024/1000/1000}
+
+                    #             else if ($9 =="Kbits/sec")
+                    #                 {print $3, $8*1024/1000/1000}
+
+                    #             else 
+                    #                 {print $3, $8*/1000/1000}
+                    #             }' |
+                    #             gnuplot -p -e 'set title "${diagram_title}" ;  set xlabel "Time (s)"; set ylabel "Throughput over the interval (MBits/s)" ;  set yrange[0:4800] ; plot  "/dev/stdin" title "iperf3 UDP throughput" with linespoints'        )
+                    #         echo ${individual_throughput_samples} > "${individual_experiment_folder}/throughput_samples_throughout_time.txt"
+                    #         echo "  individual_throughput_samples:"
+                    #         echo "${individual_throughput_samples}"
+                    #         echo "  --------------------------------"
+
+                    #     fi
+                    # fi
+
+                    # printf "  --------------------------------\n"
+                    # printf "\n\n\n"
                 done
             done
         done
--- testing-scripts/test_multi-client_maximum-UDP-iperf-troughput_via-B/visualise.py
+++ testing-scripts/test_multi-client_maximum-UDP-iperf-troughput_via-B/visualise.py
@@ -11,6 +11,7 @@ import os
 from os.path import isfile, join
 import csv
 from statistics import *
+import json
 
 
 
@@ -24,7 +25,9 @@ with open('summary_of_results.csv', 'w', newline='') as summary_of_results_file:
     results_writer.writerow(["number_of_experiments:", number_of_experiments])
     results_writer.writerow(["delay_ms", "probability_of_loss_percentage", "payload_size", "clients_requests",      
         "min_receiver_throughput_megabits_per_second", "max_receiver_throughput_megabits_per_second", "avg_receiver_throughput_megabits_per_second", "stdev_receiver_throughput_megabits_per_second",
-        "min_completion_time_in_miliseconds", "max_completion_time_in_miliseconds", "avg_completion_time_in_miliseconds", "stdev_completion_time_in_miliseconds",
+        "min_completion_time_in_miliseconds"         , "max_completion_time_in_miliseconds"         , "avg_completion_time_in_miliseconds"         , "stdev_completion_time_in_miliseconds",
+        "min_lost_percent"                           , "max_lost_percent"                           , "avg_lost_percent"                           , "stdev_lost_percent",
+        "min_emitted_bytes"                          , "max_emitted_bytes"                          , "avg_emitted_bytes"                          , "stdev_emitted_bytes" 
         ])
 
     # 1. Extract data
@@ -42,46 +45,73 @@ with open('summary_of_results.csv', 'w', newline='') as summary_of_results_file:
 
                     list_receiver_throughput_megabits_per_second = []
                     list_completion_time_in_miliseconds = []
+                    list_lost_percent = []
+                    list_emitted_bytes=[]
+
                     
 
                     for experiment_number in range(1,number_of_experiments+1):
-                        # print("----")
-                        # print ("  experiment_number              : ", experiment_number)
-                        # print ("  payload_size                   : ", payload_size)
-                        # print ("  probability_of_loss_percentage : ", probability_of_loss_percentage)
-                        # print ("  delay-ms                       : ", delay_ms)
-                        # print ("  clients_requests               : ", clients_requests)
+                        print("----")
+                        print ("  experiment_number              : ", experiment_number)
+                        print ("  payload_size                   : ", payload_size)
+                        print ("  probability_of_loss_percentage : ", probability_of_loss_percentage)
+                        print ("  delay-ms                       : ", delay_ms)
+                        print ("  clients_requests               : ", clients_requests)
                         
                         individual_experiment_folder= str(experiment_group_folder) + "/" + str(experiment_number)
 
-                        # According to: https://realpython.com/read-write-files-python/
-                        with open(individual_experiment_folder+'/receiver_throughput_megabytes_per_second.txt', 'r') as reader:
-                            line = reader.readline()
-                            receiver_throughput_megabits_per_second = float(line)*8
-                        print("    receiver_throughput_megabits_per_second : ", float(receiver_throughput_megabits_per_second))
-                        list_receiver_throughput_megabits_per_second.append(receiver_throughput_megabits_per_second)
 
-                        with open(individual_experiment_folder+'/completion_time.txt', 'r') as reader:
-                            line = reader.readline()
-                            completion_time_in_miliseconds = float(line)*1000
-                        print("    completion_time_in_miliseconds : ", float(completion_time_in_miliseconds))
-                        list_completion_time_in_miliseconds.append(completion_time_in_miliseconds)
+                        # File reading functionality is written according instructions from:
+                        # https://stackabuse.com/reading-and-writing-json-to-a-file-in-python/
+                        # https://realpython.com/read-write-files-python/
+                        with open(individual_experiment_folder+'/result.json') as results_file:
+                            experiment_data = json.load(results_file)
+                            experiment_summary = experiment_data['end']['sum']
 
+                            receiver_throughput_megabits_per_second = float(experiment_summary['bits_per_second'])/1000000
+                            list_receiver_throughput_megabits_per_second.append(receiver_throughput_megabits_per_second)
+
+                            completion_time_in_miliseconds = float(experiment_summary['seconds'])*1000
+                            list_completion_time_in_miliseconds.append(completion_time_in_miliseconds)
+
+                            lost_percent = float(experiment_summary['lost_percent'])
+                            list_lost_percent.append(lost_percent)
+
+                            emitted_bytes = float(experiment_summary['bytes'])
+                            list_emitted_bytes.append(emitted_bytes)
+
+                            print("    completion_time_in_miliseconds          : ", float(completion_time_in_miliseconds))
+                            print("    receiver_throughput_megabits_per_second : ", float(receiver_throughput_megabits_per_second))
+                            print("    lost_percent                            : ", float(lost_percent))
+                            print("    emitted_bytes                           : ", float(emitted_bytes))
 
 
                     #1.1 Summarise data
-                    min_receiver_throughput_megabits_per_second = max(list_receiver_throughput_megabits_per_second)  
-                    max_receiver_throughput_megabits_per_second = min(list_receiver_throughput_megabits_per_second)   
-                    avg_receiver_throughput_megabits_per_second = mean(list_receiver_throughput_megabits_per_second)  
+                    min_receiver_throughput_megabits_per_second   = max(list_receiver_throughput_megabits_per_second)  
+                    max_receiver_throughput_megabits_per_second   = min(list_receiver_throughput_megabits_per_second)   
+                    avg_receiver_throughput_megabits_per_second   = mean(list_receiver_throughput_megabits_per_second)  
                     stdev_receiver_throughput_megabits_per_second = stdev(list_receiver_throughput_megabits_per_second)  
 
 
-                    min_completion_time_in_miliseconds = max(list_completion_time_in_miliseconds)  
-                    max_completion_time_in_miliseconds = min(list_completion_time_in_miliseconds)   
-                    avg_completion_time_in_miliseconds = mean(list_completion_time_in_miliseconds)  
+                    min_completion_time_in_miliseconds   = max(list_completion_time_in_miliseconds)  
+                    max_completion_time_in_miliseconds   = min(list_completion_time_in_miliseconds)   
+                    avg_completion_time_in_miliseconds   = mean(list_completion_time_in_miliseconds)  
                     stdev_completion_time_in_miliseconds = stdev(list_completion_time_in_miliseconds)  
+
+                    min_lost_percent   = max(list_lost_percent)  
+                    max_lost_percent   = min(list_lost_percent)   
+                    avg_lost_percent   = mean(list_lost_percent)  
+                    stdev_lost_percent = stdev(list_lost_percent)  
+
+
+                    min_emitted_bytes   = max(list_emitted_bytes)  
+                    max_emitted_bytes   = min(list_emitted_bytes)   
+                    avg_emitted_bytes   = mean(list_emitted_bytes)  
+                    stdev_emitted_bytes = stdev(list_emitted_bytes)  
 
                     #1.2 Write summarised data
                     results_writer.writerow([delay_ms, probability_of_loss_percentage, payload_size, clients_requests,
                         min_receiver_throughput_megabits_per_second, max_receiver_throughput_megabits_per_second, avg_receiver_throughput_megabits_per_second, stdev_receiver_throughput_megabits_per_second,
-                        min_completion_time_in_miliseconds,              max_completion_time_in_miliseconds             , avg_completion_time_in_miliseconds             , stdev_completion_time_in_miliseconds ])
+                        min_completion_time_in_miliseconds         , max_completion_time_in_miliseconds         , avg_completion_time_in_miliseconds         , stdev_completion_time_in_miliseconds,
+                        min_lost_percent                           , max_lost_percent                           , avg_lost_percent                           , stdev_lost_percent,
+                        min_emitted_bytes                          , max_emitted_bytes                          , avg_emitted_bytes                          , stdev_emitted_bytes             ])
