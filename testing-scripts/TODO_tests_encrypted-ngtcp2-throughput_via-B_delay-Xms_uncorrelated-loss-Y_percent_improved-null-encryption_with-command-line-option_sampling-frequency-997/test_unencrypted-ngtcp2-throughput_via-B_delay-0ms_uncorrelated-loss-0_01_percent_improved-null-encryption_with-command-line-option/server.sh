3a507a6a435210bed11fb6da3bcc49bcba34a241
nf-test104 <nf-test104>
2021-05-12 22:47:35 +0100

    Renamed encrypted ngtcp2 tests

--- testing-scripts/TODO_tests_encrypted-ngtcp2-throughput_via-B_delay-Xms_uncorrelated-loss-Y_percent_improved-null-encryption_with-command-line-option_sampling-frequency-997/test_unencrypted-ngtcp2-throughput_via-B_delay-0ms_uncorrelated-loss-0_01_percent_improved-null-encryption_with-command-line-option/server.sh
+++
@@ -1,6 +0,0 @@
-ngtcp2_folder="/root/evaluation/unencrypted_stack/ngtcp2"
-echo "ngtcp2_folder: ${ngtcp2_folder}"
-
-echo "Running the following command:"
-echo "taskset -c 0 ip netns exec mr_server ${ngtcp2_folder}/examples/server  -q --max-dyn-length 4g -P 10.2.2.101 7777  ${ngtcp2_folder}/examples/server.key ${ngtcp2_folder}/examples/server.cert"
-taskset -c 0 ip netns exec mr_server ${ngtcp2_folder}/examples/server  -q --max-dyn-length 4g -P 10.2.2.101 7777  ${ngtcp2_folder}/examples/server.key ${ngtcp2_folder}/examples/server.cert
